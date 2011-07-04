e2ac9b0056a9429379022cef0fa94fe2ee81fe7f
Tony Milne <t.milne.09@aberdeen.ac.uk>
2011-07-04 15:21:37 +0100

    added reproduction feature

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -16,15 +16,28 @@ class Life
     #   if 3 neighbours, live cell stays alive
     
     new_grid = []
+    h = Hash.new
     row_num = 0 # y
     @grid.each do |r|
       new_row = []
       col_num = 0 # x
       r.each do |c|
         if c.alive?
-          #check neighbours
+          # check live neighbours to see if cell should die of overcrowding or
+          # isolation
           num_alive = check_live_neighbours(row_num, col_num)
-          dead_neighbours = get_dead_neighbours
+          
+          # check dead neighbours to see if cell should reproduce
+          dead_neighbours = get_dead_neighbours(row_num, col_num)
+          
+          dead_neighbours.each do |c|
+            if h.key? c
+              cells = h[c]
+              cells << [row_num, col_num]
+            else
+              h[c] = [[row_num, col_num]]
+            end
+          end
           
           if((num_alive < 2) || (num_alive >= 4))
             #c.toggle_state #alive toggles to dead
@@ -41,13 +54,22 @@ class Life
       new_grid << new_row
       row_num += 1
     end
+    
+    h.each do |k, v|
+      if v.length == 3
+        row = k[0]
+        col = k[1]
+        new_grid[row][col] = Cell.new("x")
+      end
+    end
+    
     @grid = new_grid
   end
   
   def check_live_neighbours(row, col)
     num_alive = 0
-    check_neighbours(row, col) do
-      if @c.alive?
+    check_neighbours(row, col) do |c|
+      if c.alive?
         num_alive += 1
       end
     end
@@ -56,9 +78,9 @@ class Life
   
   def get_dead_neighbours(row, col)
     dead_cells = []
-    check_neighbours(row, col) do
-      if !@c.alive?
-        dead_cells << @c
+    check_neighbours(row, col) do |c, coord|
+      if !c.alive?
+        dead_cells << coord
       end
     end
     return dead_cells
@@ -66,38 +88,38 @@ class Life
   
   def check_neighbours(row, col)
     if (row-1) >= 0
-      @c = @grid[row-1][col]
-      yield
+      c = @grid[row-1][col]
+      yield c, [(row-1), col]
       if (col-1) >= 0
-        @c = @grid[row-1][col-1]
-        yield
+        c = @grid[row-1][col-1]
+        yield c, [(row-1),(col-1)]
       end
       if (col+1) < @grid[0].length
-        @c = @grid[row-1][col+1]
-        yield
+        c = @grid[row-1][col+1]
+        yield c, [(row-1),(col+1)]
       end
     end
     
     if (col-1) >= 0
-      @c = @grid[row][col-1]
-      yield
+      c = @grid[row][col-1]
+      yield c, [row, (col-1)]
     end
     
     if (col+1) < @grid[0].length
-      @c = @grid[row][col+1]
-      yield
+      c = @grid[row][col+1]
+      yield c, [row, (col+1)]
     end
     
     if (row+1) < @grid.length
-      @c = @grid[row+1][col]
-      yield
+      c = @grid[row+1][col]
+      yield c, [(row+1), col]
       if (col-1) >= 0
-        @c = @grid[row+1][col-1]
-        yield
+        c = @grid[row+1][col-1]
+        yield c, [(row+1), (col-1)]
       end
       if (col+1) < @grid[0].length
-        @c = @grid[row+1][col+1]
-        yield
+        c = @grid[row+1][col+1]
+        yield c, [(row+1), (col+1)]
       end
     end
   end
